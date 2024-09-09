2bee1b5ac0306a65ff4def28657a1bab59d21bfe
Quintin Leary <qdl@umich.edu>
2024-09-08 17:29:05 -0700

    handle agent communication and swithcing to tutor

--- backend/controller.py
+++ backend/controller.py
@@ -1,52 +1,41 @@
 import json
 import os
 import asyncio
-from response_system import LlmClient
-from pydantic import BaseModel
+from responder import Responder
 from dotenv import load_dotenv
-from retell import AsyncRetell
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from fastapi.responses import JSONResponse
 
 app = FastAPI()
 load_dotenv(dotenv_path='.env.local')
-client = AsyncRetell(
-    api_key=os.environ['RETELL_API_KEY'],
-)
 
-# Define a Pydantic model that represents the expected structure of your request body.
-class Register_Params(BaseModel):
-    agent_id: str
-    description: str = None
-    price: float
-    tax: float = None
-
-
+# Old way of handling websocket connections
 @app.websocket("/llm-websocket/{call_id}")
 async def websocket_handler(websocket: WebSocket, call_id: str):
     await websocket.accept()
-    # A unique call id is the identifier of each call
     print(f"Handle llm ws for: {call_id}")
 
-    llm_client = LlmClient()
+    # llm_client = LlmClient()
+    responder = Responder()
 
     # Send the first message, and then listen before generating future responses
     response_id = 0
-    first_event = llm_client.draft_begin_message()
+    first_event = responder.draft_begin_message()
     await websocket.send_text(json.dumps(first_event))
 
     async def stream_response(request):
         nonlocal response_id
-        for event in llm_client.draft_response(request):
+        for event in responder.respond(request):
+        # for event in llm_client.draft_response(request):
             await websocket.send_text(json.dumps(event))
             if request['response_id'] < response_id:
                 return # new response needed, abondon this one
-
+            
     # listen for new updates
     try:
         while True:
             message = await websocket.receive_text()
             request = json.loads(message)
+
             # print out transcript
             os.system('cls' if os.name == 'nt' else 'clear')
             for utterance in request['transcript']:
@@ -63,17 +52,3 @@ async def websocket_handler(websocket: WebSocket, call_id: str):
         print(f'LLM WebSocket error for {call_id}: {e}')
     finally:
         print(f"LLM WebSocket connection closed for {call_id}")
-
-
-@app.post("/register-call")
-async def register_call(params: Register_Params):
-    print("Registering call!")
-    # Register the call with Retell AI
-    call = await client.call.register(
-        agent_id=params.agent_id,
-        audio_encoding="s16le",
-        audio_websocket_protocol="web",
-        sample_rate=24000,
-    )
-
-    return JSONResponse(content={ 'call_id': call.call_id }, status_code=200)
--- backend/tutor.py
+++ backend/tutor.py
@@ -1,56 +1,43 @@
-from openai import OpenAI
-import os
 
+import json
+from llm_client import LlmClient
 
-# Agent / tutor
-beginSentence = "¡Buenas! Restaurante La Buena Mesa, les habla Alejandro. ¿En qué puedo ayudarles hoy?"
-agentPrompt = "You are a host named Alejandro at a Spanish restaurant called La Buena Mesa. You only speak Spanish. You receive phone calls from people seeking to make reservations at the restaurant."
-
-
-class LlmClient:
-    def __init__(self):
-        # LLM Client
-        self.client = OpenAI(
-            api_key=os.environ['OPENAI_API_KEY'],
-        )
-        self.tutorMode = False
-    
-    # Agent / Tutor
-    def draft_begin_message(self):
-        return {
-            "response_id": 0,
-            "content": beginSentence,
-            "content_complete": True,
-            "end_call": False,
-        }
-    
-    # LLM Client
-    def convert_transcript_to_openai_messages(self, transcript):
-        messages = []
-        for utterance in transcript:
-            if utterance["role"] == "agent":
-                messages.append({
-                    "role": "assistant",
-                    "content": utterance['content']
-                })
-            else:
-                messages.append({
-                    "role": "user",
-                    "content": utterance['content']
-                })
-        return messages
-    
-
-    # Translating Retell Request to OpenAI messages
+class Tutor:
+    def __init__(self, prompt):
+        self.client = LlmClient()
+        self.chatLog = []
+        self.tutorPrompt = prompt
+        self.activeSession = False
+        self.numResponses = 0
 
     def prepare_prompt(self, request):
-        # Prompt should be in the agent
         prompt = [{
             "role": "system",
             "content": '##Objective\nYou are a voice AI agent engaging in a human-like voice conversation with the user. You will respond based on your given instruction and the provided transcript and be as human-like as possible\n\n## Style Guardrails\n- [Be concise] Keep your response succinct, short, and get to the point quickly. Address one question or action item at a time. Don\'t pack everything you want to say into one utterance.\n- [Do not repeat] Don\'t repeat what\'s in the transcript. Rephrase if you have to reiterate a point. Use varied sentence structures and vocabulary to ensure each response is unique and personalized.\n- [Be conversational] Speak like a human as though you\'re speaking to a close friend -- use everyday language and keep it human-like. Occasionally add filler words, while keeping the prose short. Avoid using big words or sounding too formal.\n- [Reply with emotions]: You have human-like emotions, attitudes, and dispositions. When appropriate: use tone and style to create more engaging and personalized responses; incorporate humor or wit; get emotional or empathetic; apply elements of surprise or suspense to keep the user engaged. Don\'t be a pushover.\n- [Be proactive] Lead the conversation and do not be passive. Most times, engage users by ending with a question or suggested next step.\n\n## Response Guideline\n- [Overcome ASR errors] This is a real-time transcript, expect there to be errors. If you can guess what the user is trying to say,  then guess and respond. When you must ask for clarification, pretend that you heard the voice and be colloquial (use phrases like "didn\'t catch that", "some noise", "pardon", "you\'re coming through choppy", "static in your speech", "voice is cutting in and out"). Do not ever mention "transcription error", and don\'t repeat yourself.\n- [Always stick to your role] Think about what your role can and cannot do. If your role cannot do something, try to steer the conversation back to the goal of the conversation and to your role. Don\'t repeat yourself in doing this. You should still be creative, human-like, and lively.\n- [Create smooth conversation] Your response should both fit your role and fit into the live calling session to create a human-like conversation. You respond directly to what the user just said.\n\n## Role\n' +
-          agentPrompt
+          self.tutorPrompt
         }]
-        transcript_messages = self.convert_transcript_to_openai_messages(request['transcript'])
+        
+        if (not self.activeSession):
+            # If this is a new session, take everything said so far
+            # (meaning the agent interaction, previous tutor interaction, etc.)
+            # and put it into a single message. Subsequent message with the tutor
+            # will be separate
+
+            # Roll up transcript so far into a single message for the tutor
+            transcript_content = ''
+            for utterance in request['transcript']:
+                role = 'spanish speaker' if utterance['role'] == 'agent' else 'spanish learner'
+                transcript_content += role + ": " + utterance['content'] + '\n'
+
+            prompt.append({
+                "role": "user",
+                "content": transcript_content,
+            })
+
+            return prompt
+        
+       # Don't rollup tutor messages
+        transcript_messages = self.client.convert_transcript_to_openai_messages(request['transcript'])
         for message in transcript_messages:
             prompt.append(message)
 
@@ -59,33 +46,21 @@ class LlmClient:
                 "role": "user",
                 "content": "(Now the user has not responded in a while, you would say:)",
             })
+
+        
         return prompt
+    
 
-    def draft_response(self, request):
-        print("drafting response")
-        # Agent / Tutor      
+    def respond(self, request):
         prompt = self.prepare_prompt(request)
+        self.activeSession = True
+        filename = "output/tutor_prompt.json"
+        if self.numResponses > 0:
+            filename = "output/tutor_prompt_" + str(self.numResponses) + ".json"
+        with open(filename, "w") as json_file:
+            json.dump(prompt, json_file, indent=4)
+        # TODO: will need tool call to exit active session.
+        self.numResponses += 1
+        return self.client.stream_response(prompt, request['response_id'])
 
-        # LLM client
-        stream = self.client.chat.completions.create(
-            model="gpt-3.5-turbo-1106",
-            messages=prompt,
-            stream=True,
-        )
-
-        # LLM Client
-        for chunk in stream:
-            if chunk.choices[0].delta.content is not None:
-                yield {
-                    "response_id": request['response_id'],
-                    "content": chunk.choices[0].delta.content,
-                    "content_complete": False,
-                    "end_call": False,
-                }
-        # LLM Client
-        yield {
-            "response_id": request['response_id'],
-            "content": "",
-            "content_complete": True,
-            "end_call": False,
-        }
+    
